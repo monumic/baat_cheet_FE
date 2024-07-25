0410f57e24babd93d9c4ce42f3562cfc5ff09880
monumic <128798766+monumic@users.noreply.github.com>
2024-07-25 22:57:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://baatcheet-be.onrender.com';
+  axios.defaults.baseURL = 'https://baat-cheet-be.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
